9e1e54cbb5e31481e4474a045d2f25de3e9c6ab5
Akash <phalke.aka@gmail.com>
2022-12-05 19:44:17 +0530

    update

--- sample.tf
+++ sample.tf
@@ -5,3 +5,11 @@ default = "hello world"
 output "sample" {
     value = var.sample
 }
+
+variable "sample1" {
+default = "i am decalring sample 1"
+}
+
+output "sample1"{
+    value = sample1.var
+}
