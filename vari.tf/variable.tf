1929e82d07f42d5fe2a30058e18cceb6032ea198
Akash <phalke.aka@gmail.com>
2022-12-07 19:37:54 +0530

    update

--- vari.tf/variable.tf
+++ vari.tf/variable.tf
@@ -11,5 +11,13 @@ output "sample1" {
 }
 
 output "sample2" {
-    value = "this is second sample ${var.sample2}"
+    value = "this is second sample ${var.sample}"
+}
+
+variable "number" {
+default = 100
+}
+
+output "number" {
+    value = var.number
 }
