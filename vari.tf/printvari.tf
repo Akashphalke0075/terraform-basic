30f1dc65c320796cadee94f1ae3d01005330604a
Akash <phalke.aka@gmail.com>
2022-12-08 17:10:43 +0530

    update

--- vari.tf/printvari.tf
+++ vari.tf/printvari.tf
@@ -11,4 +11,3 @@ variable "ex-list"  {
 output "ex-list-output" {
     value = "welcome to ${var.ex-list[0]} ${var.ex-list[1]} tarining with ${var.ex-list[2]} percent ${var.ex-list[3]} by ${var.ex-list[4]}" 
 }
-1
